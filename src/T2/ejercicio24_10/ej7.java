a9a29d6de0c7258313ffaeef369979219854c081
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-10-26 10:29:02 +0200

    26/10/2017 clase prog

---
+++ src/T2/ejercicio24_10/ej7.java
@@ -0,0 +1,13 @@
+package T2.ejercicio24_10;
+import java.util.Scanner;
+
+public class ej7 {
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Introduzca la nota por pantalla");
+		
+		
+	}
+
+}
