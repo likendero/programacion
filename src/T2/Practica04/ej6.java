a0b35ae136792defbf87fea537cdafcdf300dcaa
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-11-05 18:48:17 +0100

    gh

--- src/T2/Practica04/ej6.java
+++ src/T2/Practica04/ej6.java
@@ -1,4 +1,10 @@
 package T2.Practica04;
 
 public class ej6 {
+    public static void main (String[] args){ int x = 2, y = 2, z;
+        z = x*y + 10 + 4 / 2 - 2 * 4 + 2; System.out.print("El resultado es ");
+        System.out.print(z);
+    }
+
 }
+
