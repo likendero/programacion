5572d78671e51ecfd8733dd9ef81bc9a34431b6e
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-11-06 17:45:04 +0100

    ej10

---
+++ src/T2/Practica04/ej10.java
@@ -0,0 +1,31 @@
+package T2.Practica04;
+
+import java.util.Scanner;
+
+/**
+ * @Author Sergey Shevchenko
+ */
+public class ej10 {
+    /*
+    Programa que lee un número de 3 cifras y muestra sus
+    cifras por separado.  Lee 315 y muestra 3 1 5
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        int num=0;
+        while (num<100|| num>999){
+            System.out.println("Introduzca un numero entero de 3 cifras");
+            num=teclado.nextInt();
+            if (num<100|| num>999)
+                System.out.println("No es un valor valido");
+        }
+        System.out.print("Las cifras que forman el numero "+num+" son ");
+        int cont=2, cif;
+        while(cont>=0){
+            cif= (int) (num/Math.pow(10,cont));
+            num= (int) (num-cif*Math.pow(10,cont));
+            System.out.print(cif+" ");
+            cont--;
+        }
+    }
+}
---
+++ src/T2/Practica04/ej9.java
@@ -0,0 +1,30 @@
+package T2.Practica04;
+
+import java.util.Scanner;
+
+/**
+ * @Author Sergey Shevchenko
+ */
+public class ej9 {
+    /*
+    Realiza un programa Java que lea un número por teclado
+    que pida el precio de un producto (puede tener decimales)
+    y calcule el precio final con IVA. El IVA será una constante
+    que será del 21%.
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        final int IVA=21;
+
+        System.out.println("Introduzca el precio de producto.");
+        float precio=0;
+        while (precio<=0) {
+            precio = teclado.nextFloat();
+            if (precio <= 0)
+                System.out.println("No es un valor valido");
+        }
+        System.out.println("El precio del producto con IVA es de "+(precio+precio*21/100));
+
+        teclado.close();
+    }
+}
