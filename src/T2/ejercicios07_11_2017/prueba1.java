582384facb2c500ffc0796fff44903d179d1ca9b
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-11-07 18:09:29 +0100

    pruebas

--- src/T2/ejercicios07_11_2017/prueba1.java
+++ src/T2/ejercicios07_11_2017/prueba1.java
@@ -3,6 +3,7 @@ package T2.ejercicios07_11_2017;
 public class prueba1 {
     public static void main(String[] args) {
 
+        System.out.println((float) 4/4/4*5);
         int i;
         for(i=0;i<10;i++) {
             System.out.println("Hola");
