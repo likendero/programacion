748b1f4cf317aacde511db76552ee4a7fba0f7fa
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-11-13 11:26:48 +0100

    ejercicios de clase

---
+++ src/T2/ejercicios13_11_2017/PruebaEj2.java
@@ -0,0 +1,17 @@
+package T2.ejercicios13_11_2017;
+
+public class PruebaEj2 {
+    public static void main(String[] args) {
+        int velocidad=120;
+        double consumo;
+        if (velocidad > 80)
+            consumo = 10.00;
+        else
+        if (velocidad > 100)
+            consumo = 12.00;
+        else
+        if (velocidad > 120)
+            consumo = 15.00;
+    }
+
+}
---
+++ src/T2/ejercicios13_11_2017/PruebaEj3.java
@@ -0,0 +1,7 @@
+package T2.ejercicios13_11_2017;
+
+public class PruebaEj3 {
+    public static void main(String[] args) {
+
+    }
+}
