ddc673be90a140db4e8fd94c946b11d4eef4000d
dragongrox <31999215+dragongrox@users.noreply.github.com>
2017-11-16 18:48:32 +0100

    Ejercicio 2 y 3 de la practica obligatoria 2 T3
    Magicamente el metodo que evalua la hora del ejercicio 3 no funciona correctamente porque java se niega a evaluarlo correctamente ¬¬

--- src/T3/Practica02_obligatoria/Ejercicio2.java
+++ src/T3/Practica02_obligatoria/Ejercicio2.java
@@ -16,6 +16,28 @@ estos resultados al usuario.
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
+        float num=1, sumaImpares=0, sumaPares=0;
+        int contI=0, contP=0;
+        while (num!=0){
+            System.out.println("Introduzca el valor ");
+            num=entrada.nextFloat();
+            if (num!=0){
+                if (num%2==0) {
+                    sumaPares = sumaPares + num;
+                    contP++;
+                }
+                else {
+                    sumaImpares = sumaImpares + num;
+                    contI++;
+                }
+            }
+        }
+        System.out.println("La suma de todos los pares es "+sumaPares);
+        System.out.println("El porcentaje de los numero pares es de "+(contP*100/(contI+contP))+"%");
+        System.out.println("La suma de todos los impares es "+sumaImpares);
+        System.out.println("El porcentaje de los numeros impares es de "+(100-contP*100/(contI+contP))+"%");
+        System.out.println("La suma de todos los numeros es de "+(sumaImpares+sumaPares));
+        System.out.println("En total fueron ingresados "+(contI+contP));
 
     }
 }
---
+++ src/T3/Practica02_obligatoria/Ejercicio3.java
@@ -0,0 +1,33 @@
+package T3.Practica02_obligatoria;
+
+import java.util.Scanner;
+
+/**
+ * @Author Sergey Shevchenko
+ */
+/*
+Escribir un programa que lea la hora en notación de 24 horas y que
+imprima en notación de 12; por ejemplo, si la entrada es 13:45, la salida
+será 1:45 pm. El programa debe solicitar al usuario que introduzca
+exactamente cinco caracteres para especificar una hora; por ejemplo, las
+9 en punto se debe introducir así: 09:00.
+ */
+public class Ejercicio3 {
+    public static boolean comprobacionHora(String hora) {
+        if(hora.length()!=5||hora.charAt(2)!=':'||Integer.valueOf(hora.substring(0,2))>23||Integer.valueOf(hora.substring(0,2))<0||Integer.valueOf(hora.substring(3,4))>59||Integer.valueOf(hora.substring(3,4))<0)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        String hora;
+        do {
+            System.out.println("Introduzca la hora");
+            hora=entrada.next();
+            if (comprobacionHora(hora)==false)
+                System.out.println("No es una hora valida.");
+        }while (comprobacionHora(hora)==false);
+    }
+}
